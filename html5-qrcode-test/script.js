3f97a5f8afcc72bfec1d39db48cad71553d071ee
Dimitris Tsamitros <dimitris.tsamitros@internations.org>
2023-09-12 14:34:16 +0300

    Write the last code read

--- html5-qrcode-test/script.js
+++ html5-qrcode-test/script.js
@@ -1,22 +1,3 @@
-console.log('Hello!!!');
-
-function onScanSuccess(decodedText, decodedResult) {
-    // handle the scanned code as you like, for example:
-    console.log(`Code matched = ${decodedText}`, decodedResult);
-}
-
-function onScanFailure(error) {
-    // handle scan failure, usually better to ignore and keep scanning.
-    // for example:
-    console.warn(`Code scan error = ${error}`);
-}
-
-// let html5QrcodeScanner = new Html5QrcodeScanner(
-//   'reader',
-//   { fps: 10, qrbox: { width: 250, height: 250 } },
-//   /* verbose= */ false
-// );
-// html5QrcodeScanner.render(onScanSuccess, onScanFailure);
 
 const html5QrCode = new Html5Qrcode(/* element id */ 'reader');
 html5QrCode
@@ -27,7 +8,7 @@ html5QrCode
             qrbox: { width: 250, height: 250 }, // Optional, if you want bounded box UI
         },
         (decodedText, decodedResult) => {
-            // do something when code is read
+            document.getElementById("last-code-scanned").innerHTML = decodedText;
         },
         (errorMessage) => {
             // parse error, ignore it.
